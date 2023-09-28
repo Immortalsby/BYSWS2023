97f5b546edd432f4ded26d58609c78159d6707a7
Sbysbys666 <sby913945@gmail.com>
2023-09-28 19:17:41 +0200

    product

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'dumi';
 
-const repo = 'BYSWS2023';
+// const repo = 'BYSWS2023';
 
 export default defineConfig({
 
-  title: 'BYSWS2023',
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  // title: 'BYSWS2023',
+  // base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
   themeConfig: {
     name: "BY.S",
-    logo: "/logo.png",
+    logo: "logo.png",
     footer: "Copyright © 2022-present | Powered by self",
     socialLinks: {
       github: 'https://github.com/Immortalsby/BYSWS2023',
