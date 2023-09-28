4881f3421d7d5e9acd8d979fc52671a9538ac239
Sbysbys666 <sby913945@gmail.com>
2023-09-28 17:46:44 +0200

    first commit

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,10 +5,6 @@ const repo = 'BYSWS2023';
 export default defineConfig({
 
   title: 'BYSWS2023',
-  mode: 'site',
-  devServer: {
-    port: 1998 // 自定义端口号
-  },
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
